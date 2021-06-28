4b3ae47c6378c1b64f37223d438bccdf1cda48e9
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-28 11:01:31 +0530

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,42 +1,65 @@
 from __future__ import print_function
 import os.path
+import sqlalchemy as db
+from sqlalchemy import Table, Column, Integer, String
+from requests import Session
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+engine = db.create_engine('sqlite:///gmail.db', echo=True)
+meta = db.MetaData()
 
-def get_service():
 
+mail = Table(
+   'mail', meta,
+   Column('id', Integer, primary_key=True),
+   Column('description', String),
+   Column('thID', String),
+)
+session = Session()
+meta.create_all(engine)
+
+
+def get_gmail_service():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.vald:
+    if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'client.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-
-
-    service = build('gmail', 'v1' , credentials =creds)
+    service = build('gmail', 'v1', credentials=creds)
     return service
-     
-def get_email_all():
+
+def get_email_list():
     service = get_gmail_service()
-    results = service.users().messages().list(userId= 'me' ,maxResults=5).execute()
-    messages = results.get('messages', [])
-    for message in messages:
-        msg = service.users().messages().get(userId='me', id=message['id']).execute()
-        print(msg['snippet'])
-        print(msg['id'])
-    
-   
+    results=service.users().messages().list(userId='me',maxResults=5).execute()
+    return results.get('messages',[])
+
+def get_email_content(message_id):
+    service=get_gmail_service()
+    results = service.users().messages().get(userId='me', id=message_id).execute()
+    data = {'snippet': results['snippet'],'threadId': results['threadId']}
+    return data
+
+def store():
+    engine = db.create_engine('sqlite:///gmail.db', echo=True)
+    conn = engine.connect()
+    result = get_email_content('17a50ab54502b678')
+    conn.execute('INSERT INTO mail(description,thId) VALUES(:description,:thId)',
+                 (result['snippet']), result['threadId'])
+    print("entered successfully")
+    conn.close()
+
 
 if __name__ == '__main__':
-    get_email()
+    store()
